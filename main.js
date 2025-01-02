d172d674f06b2f1df0221e785acb3639782dff39
adenine-dot <shafqatadina13@gmail.com>
2025-01-02 23:18:34 +0500

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,22 @@ window.onload = function() {
         button.addEventListener('click', changeButtonColor); 
     });
 };
+document.addEventListener("DOMContentLoaded", () => {
+    const images = [
+        "a pink and purple beauty salon.png",
+        "a woman getting her haircut in a pink and purple salon.png",
+        "a woman getting facial in a pink and purple beauty salon.png",
+        "a woman getting a manicure in a pink and purple salon.png",
+        "woman getting her makeup done in a pink and purple salon.png"
+    ];
+
+    let currentIndex = 0;
+    const imgElement = document.querySelector('.rel img');
+    
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        imgElement.src = images[currentIndex];
+    }
+
+    setInterval(changeImage, 3000);
+});
